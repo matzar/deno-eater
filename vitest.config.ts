74536b6e21b02bc24f99deb1ea94a5d955e2bb19
Mateusz Zaremba <mat.zar@icloud.com>
2025-07-03 08:30:00 +0100

    feat: install vitest

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    globals: true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'test/',
+        '**/*.d.ts',
+        '**/*.config.*',
+        'coverage/',
+      ],
+    },
+  },
+});
